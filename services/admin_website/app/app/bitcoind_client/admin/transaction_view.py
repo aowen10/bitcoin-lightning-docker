f49fbe8a78ff9bf269312b7d03def48247013345
Pierre Rochard <pierre@rochard.org>
2018-07-13 11:32:50 -0500

    working on transaction html

--- services/admin_website/app/app/bitcoind_client/admin/transaction_view.py
+++ services/admin_website/app/app/bitcoind_client/admin/transaction_view.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask_admin import expose, BaseView
 
 from app.bitcoind_client.bitcoind_client import BitcoinClient
@@ -17,6 +19,8 @@ class TransactionView(BaseView):
             txid = bitcoin.get_recent_txid()
 
         transaction = bitcoin.get_raw_transaction(txid)
+        blocktime_date = datetime.fromtimestamp(transaction['blocktime'])
+        transaction['blocktime'] = str(blocktime_date) + ' (' + str(transaction['blocktime']) + ')'
 
         tooltips = {
             'txid': 'An identifier used to uniquely identify a particular transaction; specifically, the sha256d hash of the transaction.',
@@ -29,7 +33,6 @@ class TransactionView(BaseView):
             'blockhash': 'Hash of the block the transaction was included in.',
             'confirmations': 'A score indicating the number of blocks on the best block chain that would need to be modified to remove or modify a particular transaction.',
             'blocktime': 'The block time is a Unix epoch time when the miner started hashing the header (according to the miner). Must be strictly greater than the median time of the previous 11 blocks. Full nodes will not accept blocks with headers more than two hours in the future according to their clock.',
-
         }
 
         metadata = [
